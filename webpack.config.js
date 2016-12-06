b126980113ca61cab93939ceca73454fa6f794a2
Marcel Fahle <m.fahle@gmail.com>
2016-12-06 14:14:23 +0100

    initual commit, adds all code from Getting Started with Redux course

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path = require("path");
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { 
+        test: /\.js$/, 
+        exclude: /node_modules/, 
+        loader: "babel-loader" 
+      }
+    ]
+  }
+}
